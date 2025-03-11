97aa6e4bc06047a798c9a1df9797013fd17448f0
홍석주 <hsj202201@gmail.com>
2025-03-11 16:58:40 +0900

    header 왼쪽 여백 문제 해결

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,13 +5,13 @@ import courtmateLogo from "../../assets/courtmate.png";
 const HeaderContainer = styled.header`
   position: fixed;
   top: 0;
+  left : 0;
   width: 100%;
   height: 90px;
   background-color: white;
   justify-content:center;
   display: flex;
   align-items: center;
-  padding: 0 20px;
   border-bottom: 1px solid #ddd;
   z-index: 100;
 `;
